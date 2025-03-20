105d4fc3bbf286ece5e09c30950d21ea5181d241
Vishal Parekh <vishalcparekh@outlook.com>
2025-03-20 11:20:12 -0500

    fix: use favicon as link preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,30 @@
-import "./globals.css";
 import { Footer, Navbar } from "components";
 import { ActiveSectionContextProvider } from "contexts";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Vishal | Personal Portfolio",
   description: "Vishal is a full-stack developer with ~3 years of experience.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Vishal | Personal Portfolio",
+    description: "Vishal is a full-stack developer with ~3 years of experience.",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 32,
+        height: 32,
+        alt: "Vishal's Portfolio",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
